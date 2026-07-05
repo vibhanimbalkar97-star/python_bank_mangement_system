bf5038214ca5e2bbde57c78202646f477a11b0be
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2026-07-05 11:43:02 +0530

    create account function done

--- main.py
+++ main.py
@@ -4,59 +4,60 @@ import random
 import string
 
 class Bank:
-   # create dummy data
-   database='data.json'
-   data=[]
-
-   # open databse file
-   try:
-      if Path(database).exists():
-         with open(database) as fs:
-            data = json.loads(fs.read())
-      else:
-         print("No such file")
-   except Exception as err:
-      print(f"An error occurred as {err}")
+   database = 'data.json'
 
    @classmethod
-   def __update(cls):
+   def __load_data(cls):
+      if Path(cls.database).exists():
+         with open(cls.database, "r") as fs:
+            try:
+               return json.load(fs)
+            except Exception as err:
+               print(f"An error occurred as {err}")
+               return []
+      return []
+   
+   @classmethod
+   def __save_data(cls, data):
       with open(cls.database, "w") as fs:
-         fs.write(json.dumps(Bank.data))
+         json.dump(data, fs, indent=4)
 
    @classmethod
-   def __generateaccountnum(cls):
-      alpha = random.choices(string.ascii_letters, k=3)
-      num = random.choices(string.digits, k=3)
-      spchar = random.choices("!@#$%^&*", k=1)
-      id = alpha + num + spchar
-      random.shuffle(id)
-      return "".join(id)
-      
-   def createaccount(self):
-      info = {
-         "name": input("Tell your name:- "),
-         "age": int(input("Tell your age:- ")),
-         "email": input("Tell your email:- "),
-         "pin": int(input("Tell your 4 number pin:- ")),
-         "accountnumber": Bank.__generateaccountnum(),
-         "balance": 0
-      }
-      if info["age"] < 18 or len(str(info["pin"])) != 4:
-         print("Sorry you can not create your account")
-      else:
-         print("Your account created successfully")
-         # show the info
-         for i in info:
-            print(f"{i} : {info[i]}")
-         print("Please note down your account number")
+   def __generate_acc_number(cls):
+      data = cls.__load_data()
+      while True:
+         chars = (random.choices(string.ascii_letters, k=3) + \
+                 random.choices(string.digits, k=3) + \
+                 random.choices("!@#$%^&*", k=1))
+         random.shuffle(chars)
+         acc_no = "".join(chars) 
          
-         Bank.data.append(info)
-
-         Bank.__update()
-user = Bank()
-
-
-
+         for user in data:
+            if user["acc_no"] == acc_no:
+               break
+         else:
+               return acc_no
+             
+             
+   
+   @classmethod
+   def create_account(cls, name, age, email, pin):
+      data = cls.__load_data()
+      if age < 18 or len(str(pin)) != 4:
+         return None, "Age must be 18+ and pin should be 4 digits"
+      acc_no = cls.__generate_acc_number()
+      user = {
+         "name":name,
+         "age":age,
+         "email":email,
+         "acc_no":acc_no,
+         "pin":pin,
+         "balance":0
+      }
+      data.append(user)
+      cls.__save_data(data)   
+      return user, "Account created successfully" 
+            
 
 print("Press 1 for creating an account:- ")
 print("Press 2 for depositing money in the bank:- ")
@@ -68,4 +69,21 @@ print("Press 6 for deleting the details:- ")
 check = int(input("Tell your response:- "))
 
 if check == 1:
-   user.createaccount() 
+   name = input("Enter your name: ")
+   age = int(input("Enter your age: "))
+   email = input("Enter your email: ")
+   pin = int(input("Enter 4-digit PIN: "))
+
+   user, message = Bank.create_account(name, age, email, pin)
+   print(message)
+   print(user)
+
+elif check == 2:
+   acc_no = input("Enter your account number")
+   pin = int(input("Enter your pin"))
+
+ 
+
+
+
+
